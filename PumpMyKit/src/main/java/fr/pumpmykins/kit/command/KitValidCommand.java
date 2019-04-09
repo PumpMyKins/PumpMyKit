2be7ef3d115c7629e290182f906da7b35d9aca54
Sufike <l.christ54260@gmail.com>
2019-04-09 23:27:21 +0200

    New Command implementation for kit protection

---
+++ PumpMyKit/src/main/java/fr/pumpmykins/kit/command/KitValidCommand.java
@@ -0,0 +1,74 @@
+package fr.pumpmykins.kit.command;
+
+import java.util.List;
+
+import fr.pumpmykins.kit.KitList;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.server.permission.PermissionAPI;
+
+public class KitValidCommand implements ICommand {
+
+	private KitList kitlist;
+	
+	public KitValidCommand(KitList kitlistinstance) {
+		this.kitlist = kitlistinstance;
+	}
+
+	@Override
+	public int compareTo(ICommand o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsage(ICommandSender sender) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getAliases() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
+		
+		if(sender instanceof EntityPlayer) {
+			return PermissionAPI.hasPermission((EntityPlayer) sender, "kit.modify");
+		}
+		return false;
+	}
+
+	@Override
+	public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args,
+			BlockPos targetPos) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isUsernameIndex(String[] args, int index) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
