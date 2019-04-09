7280ab640373772c05dc90571f4f69eb82819008
Sufike <l.christ54260@gmail.com>
2019-04-09 17:51:51 +0200

    Kit add command basics

--- PumpMyKit/src/main/java/fr/pumpmykins/kit/command/KitAddCommand.java
+++ PumpMyKit/src/main/java/fr/pumpmykins/kit/command/KitAddCommand.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 import com.google.common.collect.Lists;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.server.permission.PermissionAPI;
 
 public class KitAddCommand implements ICommand {
@@ -40,8 +44,37 @@ public class KitAddCommand implements ICommand {
 
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-		// TODO Auto-generated method stub
 
+		if(sender instanceof EntityPlayer) {
+			
+			sender = (EntityPlayer) sender;
+			
+			if(args.length > 0) {
+				
+				String kitname = args[0];
+				System.out.println(kitname);
+				BlockPos chest_pos = sender.getPosition();
+				
+				EnumFacing i = ((EntityPlayer) sender).getHorizontalFacing();
+				if(i == EnumFacing.EAST)
+					chest_pos = chest_pos.east();
+				if(i == EnumFacing.WEST)
+					chest_pos = chest_pos.west();
+				if(i == EnumFacing.SOUTH)
+					chest_pos = chest_pos.south();
+				else
+					chest_pos = chest_pos.north();
+				
+				World w = sender.getEntityWorld();
+				IBlockState chest = Blocks.CHEST.getDefaultState();
+				
+				w.setBlockState(chest_pos, chest);
+			}
+			else {
+				getUsage(sender);
+			}
+		}
+		
 	}
 
 	@Override
