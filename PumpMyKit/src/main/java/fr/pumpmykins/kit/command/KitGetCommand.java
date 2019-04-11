9842463ec4a8de2134b30c36532eb8ef33d410fa
Sufike <l.christ54260@gmail.com>
2019-04-11 11:31:39 +0200

    CommandGet Execute done perfection //TODO Message

--- PumpMyKit/src/main/java/fr/pumpmykins/kit/command/KitGetCommand.java
+++ PumpMyKit/src/main/java/fr/pumpmykins/kit/command/KitGetCommand.java
@@ -1,15 +1,27 @@
 package fr.pumpmykins.kit.command;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import com.google.common.collect.Lists;
 
+import fr.pumpmykins.kit.Kit;
 import fr.pumpmykins.kit.KitList;
+import fr.pumpmykins.kit.util.PmkStyleTable;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
 
 public class KitGetCommand implements ICommand {
 
@@ -46,8 +58,89 @@ public class KitGetCommand implements ICommand {
 
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-		// TODO Auto-generated method stub
-
+		
+		if(sender instanceof EntityPlayer) {
+			
+			EntityPlayer player = (EntityPlayer) sender;
+			
+			if(args.length < 1) {
+				
+				getUsage(sender);
+			} else {
+				
+				if(kitlist.getKit(args[0]) == null || args[0] == "list") {
+					
+					ITextComponent init = new TextComponentString("Liste des Kits : ");
+					init.setStyle(PmkStyleTable.orangeBold());
+					
+					sender.sendMessage(init);
+					
+					for(Kit k : kitlist.getKitlist()) {
+						
+						ITextComponent item = new TextComponentString(k.getName());
+						item.setStyle(PmkStyleTable.itemList());
+						sender.sendMessage(item);
+						
+					}
+				} else {
+					
+					Kit k = kitlist.getKit(args[0]);
+					
+					World w = server.getWorld(0);
+					
+					BlockPos chest_pos = new BlockPos(k.getX(), k.getY(), k.getZ());
+					
+					TileEntity te = w.getTileEntity(chest_pos);
+						
+					IItemHandler ih = te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+										
+					int inventorySpace = 0;
+					int kitsize = 0;
+					
+					List<ItemStack> lis = player.inventory.mainInventory;
+					List<Integer> emptySlot = new ArrayList<Integer>();
+					
+					for(int i = 0; i < 36; i++) {
+						
+						ItemStack is = lis.get(i);
+						if(is.isEmpty()) {
+							
+							inventorySpace++;
+							emptySlot.add(i);
+						}
+					}
+					
+					for(int i = 0; i < ih.getSlots(); i++) {
+						
+						ItemStack is = ih.getStackInSlot(i);
+						if(!is.isEmpty()) {
+							
+							kitsize++;
+						}
+					}
+					
+					Iterator<Integer> emptySl = emptySlot.iterator();
+					
+					if(inventorySpace < kitsize) {
+						
+						for(int i = 0; i < ih.getSlots(); i++) {
+								
+							ItemStack is = ih.getStackInSlot(i);
+							if(!is.isEmpty()) {
+								player.entityDropItem(is, 0F);
+							}
+						}
+					} else {
+						for(int i = 0; i < ih.getSlots(); i++) {
+							
+							if(!ih.getStackInSlot(i).isEmpty())
+								player.inventory.mainInventory.set(emptySl.next(), ih.getStackInSlot(i));
+							
+						}
+					}
+				}
+			}
+		}
 	}
 
 	@Override
@@ -58,13 +151,13 @@ public class KitGetCommand implements ICommand {
 	@Override
 	public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args,
 			BlockPos targetPos) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public boolean isUsernameIndex(String[] args, int index) {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
