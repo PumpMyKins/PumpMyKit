4f0d81ba6e9f74f35a9a548fd21614164d099652
Sufike <l.christ54260@gmail.com>
2019-04-11 23:16:53 +0200

    add the SQL support with the @Config

--- PumpMyKit/src/main/java/fr/pumpmykins/kit/MainKit.java
+++ PumpMyKit/src/main/java/fr/pumpmykins/kit/MainKit.java
@@ -3,15 +3,20 @@ package fr.pumpmykins.kit;
 import org.apache.logging.log4j.Logger;
 
 import fr.pumpmykins.kit.command.*;
+import fr.pumpmykins.kit.util.MySQL;
+import fr.pumpmykins.kit.util.MySQL.MySQLCredentials;
 import net.minecraft.world.World;
 import net.minecraft.world.storage.MapStorage;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.server.permission.DefaultPermissionLevel;
 import net.minecraftforge.server.permission.PermissionAPI;
 
@@ -30,6 +35,13 @@ public class MainKit {
 	
 	private KitList kitlistinstance;
 	
+	static MySQL mySQL;
+	public String host = "";
+	public String username = "";
+	public String password = "";
+	public String database = "";
+	public int port = 3306;
+	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		
@@ -46,6 +58,22 @@ public class MainKit {
 		PermissionAPI.registerNode("kit.buy.start", DefaultPermissionLevel.OP, "Allow OP to give a Kit to someone");
 		PermissionAPI.registerNode("kit.modify", DefaultPermissionLevel.OP, "Allow OP to modify a Kit");
 		PermissionAPI.registerNode("kit.reload", DefaultPermissionLevel.OP, "Allow OP to reload the Kit list");
+		
+		this.host = ModConfig.host;
+		this.username = ModConfig.username;
+		this.password = ModConfig.password;
+		this.database = ModConfig.database;
+		this.port = ModConfig.port;
+		
+		MySQLCredentials credentials = new MySQLCredentials(host, port, username, password, database);
+		mySQL = new MySQL(credentials);
+		mySQL.openConnection();
+		if(mySQL.isConnected()) {
+					
+			System.out.println("§aMySQL connection success.");
+			//BASE DE DONNER
+			mySQL.update("CREATE TABLE IF NOT EXISTS PmkKitTable( `id` INT NOT NULL AUTO_INCREMENT,`buyId` INT NOT NULL , `username` VARCHAR(16) NOT NULL , `kitname` VARCHAR(16) NOT NULL ,`used` BOOLEAN,PRIMARY KEY (`id`))");
+		}
 		
 	}
 	
@@ -64,6 +92,41 @@ public class MainKit {
 		event.registerServerCommand(new KitDeleteCommand(this.kitlistinstance));	
 		event.registerServerCommand(new KitGetCommand(this.kitlistinstance));
 	
+		
+		
+	}
+	
+	@SubscribeEvent
+	public static void configChange(ConfigChangedEvent event) {
+		
+		if(event.getModID().equals(MODID)) {
+			ConfigManager.sync(MODID, Config.Type.INSTANCE);
+		}
+	}
+	
+	@Config(modid = MODID)
+	public static class ModConfig{
+		
+		@Config.Name("Hostname")
+		@Config.Comment({"The MySQL hostname to login (IP)"})
+		public static String host ="";
+		
+		@Config.Name("Username")
+		@Config.Comment({"The MySQL Username to login"})
+		public static String username ="";
+		
+		@Config.Name("Password")
+		@Config.Comment({"The MySQL Password to login"})
+		public static String password ="";
+		
+		@Config.Name("Database")
+		@Config.Comment({"The MySQL database to login"})
+		public static String database ="PmkKit_Database";
+		
+		@Config.Name("Port")
+		@Config.Comment({"The MySQL Port of your server"})
+		public static int port = 3306;
+		
 		
 		
 	}
@@ -119,6 +182,14 @@ public class MainKit {
 
 	public void setKitlistinstance(KitList kitlistinstance) {
 		this.kitlistinstance = kitlistinstance;
+	}
+
+	public static MySQL getMySQL() {
+		return mySQL;
+	}
+
+	public static void setMySQL(MySQL mySQL) {
+		MainKit.mySQL = mySQL;
 	}
 	
 	
