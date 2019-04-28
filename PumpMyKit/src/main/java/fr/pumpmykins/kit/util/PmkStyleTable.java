0bb3d7c64641c632c2c5c6ff2222bd8bc9187b46
Sufike <l.christ54260@gmail.com>
2019-04-28 23:53:00 +0200

    new style

--- PumpMyKit/src/main/java/fr/pumpmykins/kit/util/PmkStyleTable.java
+++ PumpMyKit/src/main/java/fr/pumpmykins/kit/util/PmkStyleTable.java
@@ -1,7 +1,10 @@
 package fr.pumpmykins.kit.util;
 
 import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.event.ClickEvent;
+import net.minecraft.util.text.event.HoverEvent;
 
 public class PmkStyleTable {
 
@@ -26,7 +29,6 @@ public class PmkStyleTable {
 		Style s = new Style();
 		s.setBold(true);
 		s.setColor(TextFormatting.GOLD);
-		
 		return s;
 	}
 	
@@ -44,6 +46,16 @@ public class PmkStyleTable {
 		Style s = new Style();
 		s.setBold(true);
 		s.setColor(TextFormatting.DARK_RED);
+		
+		return s;
+	}
+	
+	public static Style kitSelect(String kitname) {
+		
+		Style s = new Style();
+		s.setColor(TextFormatting.DARK_BLUE);
+		s.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponentString("/kitselect "+kitname)));
+		s.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/kitselect "+kitname));
 		
 		return s;
 	}
