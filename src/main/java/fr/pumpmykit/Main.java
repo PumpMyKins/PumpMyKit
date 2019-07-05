80a7fc403194591b821e20d6404c065c445a4a86
Clem-Fern <clemfern456@gmail.com>
2019-07-05 22:58:57 +0200

    Update Main

---
+++ src/main/java/fr/pumpmykit/Main.java
@@ -0,0 +1,14 @@
+package fr.pumpmykit;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Main.MODID, name = Main.NAME, version = Main.VERSION, acceptableRemoteVersions = "*", serverSideOnly = true)
+public class Main
+{
+    public static final String MODID = "pumpmyfstore";
+    public static final String NAME = "PumpMyFStore";
+    public static final String VERSION = "1.0";
+    
+        
+
+}
