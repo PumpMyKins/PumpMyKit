31554d671f8ab1262f89d86829be7eb81cd18a41
Clem-Fern <clemfern456@gmail.com>
2019-07-09 10:43:41 +0200

    Adding Exceptions class

---
+++ src/main/java/fr/pumpmykit/exceptions/UnfoudKitException.java
@@ -0,0 +1,20 @@
+package fr.pumpmykit.exceptions;
+
+import net.minecraft.entity.player.EntityPlayerMP;
+
+public class UnfoudKitException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -358911015854596586L;
+
+	public UnfoudKitException(EntityPlayerMP player, String kitName) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public UnfoudKitException(EntityPlayerMP player) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
