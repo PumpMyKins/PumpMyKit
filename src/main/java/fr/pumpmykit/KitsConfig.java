cf4d004f25bcf54e5dfcdfcf1c616253376351f0
Clem-Fern <clemfern456@gmail.com>
2019-07-09 10:39:42 +0200

    Adding KitsConfig class

---
+++ src/main/java/fr/pumpmykit/KitsConfig.java
@@ -0,0 +1,20 @@
+package fr.pumpmykit;
+
+import net.minecraftforge.common.config.Config;
+
+@Config(modid = Main.MODID)
+public class KitsConfig {
+
+	public static String host = "localhost";
+	
+	public static int port = 3306;
+	
+	public static String username = "root";
+	
+	public static String password = "root";
+	
+	public static String database = "root";
+	
+	public static String servername = "server";
+	
+}
