4c71cfc0cfd692bc719f9ac8ba120a2ba5b21c4f
Clem-Fern <clemfern456@gmail.com>
2019-07-11 13:53:04 +0200

    Update main class ServerStarting Event

--- src/main/java/fr/pumpmykit/MainKit.java
+++ src/main/java/fr/pumpmykit/MainKit.java
@@ -21,61 +21,53 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 @Mod(modid = MainKit.MODID, name = MainKit.NAME, version = MainKit.VERSION, acceptableRemoteVersions = "*")
 public class MainKit
 {
-    public static final String MODID = "pumpmykit";
-    public static final String NAME = "PumpMyKit";
-    public static final String VERSION = "1.0";
-    
-    public static final ITextComponent CHAT_PREFIX = new TextComponentString("§l§6[§2Pump§eMy§dKit§6]§r§f ");
-    
-    public static Logger LOGGER;
-    public static ExecutorService EXEC;
-    public static KitsManager KITSMANAGER;
+	public static final String MODID = "pumpmykit";
+	public static final String NAME = "PumpMyKit";
+	public static final String VERSION = "1.0";
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        LOGGER = event.getModLog();
-        EXEC = Executors.newSingleThreadScheduledExecutor();
+	public static final ITextComponent CHAT_PREFIX = new TextComponentString("§l§6[§2Pump§eMy§dKit§6]§r§f ");
 
-        EXEC.submit(new Runnable() {
-			
-			@Override
-			public void run() {
-				
-				try {
-					MySqlCredentials credentials = new MySqlCredentials(KitsConfig.host, KitsConfig.port, KitsConfig.username, KitsConfig.password, KitsConfig.database);
-					MySql mySql = new MySql(credentials);
-					mySql.openConnection();
-					KITSMANAGER = new KitsManager(mySql);
-					LOGGER.info("KitsManager & MySql OK !");
-				} catch (ClassNotFoundException e) {
-					e.printStackTrace();
-					return;
-				} catch (SQLException e) {
-					e.printStackTrace();
-					return;
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				
-				
-			}
-		});
-        
-    }
-    
-    @EventHandler
-	public void onServerStarting(FMLServerStartingEvent event) {
-		
-		if(KITSMANAGER.isBddInit()) {
-			
-			KITSMANAGER.setKitList(KitList.getData(event.getServer().getEntityWorld()));
-			
-			event.registerServerCommand(new KitCommand());
-			
+	public static Logger LOGGER;
+	public static ExecutorService EXEC;
+	public static KitsManager KITSMANAGER;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		LOGGER = event.getModLog();
+		EXEC = Executors.newSingleThreadScheduledExecutor();
+
+		try {
+			MySqlCredentials credentials = new MySqlCredentials(KitsConfig.host, KitsConfig.port, KitsConfig.username, KitsConfig.password, KitsConfig.database);
+			MySql mySql = new MySql(credentials);
+			mySql.openConnection();
+			KITSMANAGER = new KitsManager(mySql);
+			LOGGER.info("KitsManager & MySql OK !");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return;
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
+
+
 	}
-        
+
+	@EventHandler
+	public void onServerStarting(FMLServerStartingEvent event) {
+
+		if(KITSMANAGER != null & KITSMANAGER.isBddInit()) {
+
+			KITSMANAGER.setKitList(KitList.getData(event.getServer().getEntityWorld()));
+
+			event.registerServerCommand(new KitCommand());
+
+		}
+
+	}
+
 
 }
