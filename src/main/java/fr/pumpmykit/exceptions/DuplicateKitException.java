185395075f849cb827e01cf01f9bf5c396c0403c
Clem-Fern <clemfern456@gmail.com>
2019-07-11 23:43:03 +0200

    Adding DuplicateKitException class

---
+++ src/main/java/fr/pumpmykit/exceptions/DuplicateKitException.java
@@ -0,0 +1,14 @@
+package fr.pumpmykit.exceptions;
+
+public class DuplicateKitException extends Exception {
+
+	public DuplicateKitException(String name) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2762934772493801033L;
+
+}
