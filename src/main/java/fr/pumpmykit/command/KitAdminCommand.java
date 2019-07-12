26fe1d86afabbec156c26774d007cbeb0fdb9ad3
Clem-Fern <clemfern456@gmail.com>
2019-07-12 13:19:01 +0200

    Adding utils subCommand in KitAdminCommand class

--- src/main/java/fr/pumpmykit/command/KitAdminCommand.java
+++ src/main/java/fr/pumpmykit/command/KitAdminCommand.java
@@ -65,23 +65,197 @@ public class KitAdminCommand implements ICommand {
 			txt2.setStyle(new Style().setColor(TextFormatting.RED));
 			txt.appendSibling(txt2);
 			sender.sendMessage(txt);
+
 			return;
-			
+
 		}
-		
+
 		if(args.length == 0) {
-			
+
 			this.helpSubCommand(server,sender,args);
 			return;
+
 		}
-		
-		
+
+		String subCommand = args[0];
+		if(subCommand.trim().isEmpty()) {			
+			this.helpSubCommand(server,sender,args);			
+		}else if(subCommand.equalsIgnoreCase("add")) {			
+			this.addSubCommand(server,sender,args);			
+		}else if(subCommand.equalsIgnoreCase("remove")) {			
+			this.removeSubCommand(server,sender,args);			
+		}else if(subCommand.equalsIgnoreCase("load")) {			
+			this.loadSubCommand(server,sender,args);		
+		}else if(subCommand.equalsIgnoreCase("update-content")) {			
+			this.updateContentSubCommand(server,sender,args);			
+		}else if(subCommand.equalsIgnoreCase("help")) {			
+			this.helpSubCommand(server,sender,args);			
+		}else {
+
+			this.synthaxErrorMessage(sender);
+
+		}		
+
+	}
+
+	private void updateContentSubCommand(MinecraftServer server, ICommandSender sender, String[] args) {
+
+		if(args.length != 2) {
+			this.synthaxErrorMessage(sender);
+			return;			
+		}
+
+		String name = args[1];
+		try {
+
+			EntityPlayer player = (EntityPlayer) sender;
+			MainKit.KITSMANAGER.updateContentKit(player,name);
+
+		} catch (UnfoudKitException e) {
+
+			ITextComponent txt = MainKit.CHAT_PREFIX.createCopy();
+			ITextComponent txt2 = new TextComponentString("Kit introuvable !");
+			txt2.setStyle(new Style().setColor(TextFormatting.RED));
+			txt.appendSibling(txt2);
+			sender.sendMessage(txt);
+
+			txt = new TextComponentString("Essayez : ");
+			txt.setStyle(new Style().setColor(TextFormatting.RED));
+
+			txt2 = new TextComponentString("/kit list");
+			txt2.setStyle(new Style().setBold(true).setColor(TextFormatting.DARK_BLUE).setClickEvent(new ClickEvent(Action.SUGGEST_COMMAND, "/kit list")));
+
+			txt.appendSibling(txt2);
+
+			sender.sendMessage(txt);
+
+		}
+
+	}
+
+	private void loadSubCommand(MinecraftServer server, ICommandSender sender, String[] args) {
+
+		if(args.length != 2) {
+			this.synthaxErrorMessage(sender);
+			return;			
+		}
+
+		String name = args[1];
+		try {
+
+			EntityPlayer player = (EntityPlayer) sender;
+			MainKit.KITSMANAGER.loadKit(player,name);
+
+		} catch (UnfoudKitException e) {
+
+			ITextComponent txt = MainKit.CHAT_PREFIX.createCopy();
+			ITextComponent txt2 = new TextComponentString("Kit introuvable !");
+			txt2.setStyle(new Style().setColor(TextFormatting.RED));
+			txt.appendSibling(txt2);
+			sender.sendMessage(txt);
+
+			txt = new TextComponentString("Essayez : ");
+			txt.setStyle(new Style().setColor(TextFormatting.RED));
+
+			txt2 = new TextComponentString("/kit list");
+			txt2.setStyle(new Style().setBold(true).setColor(TextFormatting.DARK_BLUE).setClickEvent(new ClickEvent(Action.SUGGEST_COMMAND, "/kit list")));
+
+			txt.appendSibling(txt2);
+
+			sender.sendMessage(txt);
+
+		}
+
+	}
+
+	private void removeSubCommand(MinecraftServer server, ICommandSender sender, String[] args) {
+
+		if(args.length != 2) {
+			this.synthaxErrorMessage(sender);
+			return;			
+		}
+
+		String name = args[1];
+		try {
+			
+			EntityPlayer player = (EntityPlayer) sender;
+			MainKit.KITSMANAGER.removeKit(name);
+			
+			ITextComponent txt = MainKit.CHAT_PREFIX.createCopy();
+			ITextComponent txt2 = new TextComponentString("Kit supprimé !");
+			txt2.setStyle(new Style().setColor(TextFormatting.AQUA));
+			txt.appendSibling(txt2);
+			sender.sendMessage(txt);
+
+		} catch (UnfoudKitException e) {
+
+			ITextComponent txt = MainKit.CHAT_PREFIX.createCopy();
+			ITextComponent txt2 = new TextComponentString("Kit introuvable !");
+			txt2.setStyle(new Style().setColor(TextFormatting.RED));
+			txt.appendSibling(txt2);
+			sender.sendMessage(txt);
+
+			txt = new TextComponentString("Essayez : ");
+			txt.setStyle(new Style().setColor(TextFormatting.RED));
+
+			txt2 = new TextComponentString("/kit list");
+			txt2.setStyle(new Style().setBold(true).setColor(TextFormatting.DARK_BLUE).setClickEvent(new ClickEvent(Action.SUGGEST_COMMAND, "/kit list")));
+
+			txt.appendSibling(txt2);
+
+			sender.sendMessage(txt);
+
+		}
+
+
+
+	}
+
+	private void addSubCommand(MinecraftServer server, ICommandSender sender, String[] args) {
+
+		if(args.length != 3) {
+			this.synthaxErrorMessage(sender);
+			return;			
+		}
+
+		String name = args[1];
+		String displayName = args[2];
+
+
+		EntityPlayer player = (EntityPlayer) sender;
+		MainKit.KITSMANAGER.addKit(player,name,displayName);
 
 	}
 
 	private void helpSubCommand(MinecraftServer server, ICommandSender sender, String[] args) {
-		// TODO Auto-generated method stub
-		
+
+		if(args.length != 1) {
+
+
+			return;
+
+		}
+
+	}
+
+	private void synthaxErrorMessage(ICommandSender sender) {
+
+		ITextComponent txt = MainKit.CHAT_PREFIX.createCopy();
+		ITextComponent txt2 = new TextComponentString("Erreur synthaxe !");
+		txt2.setStyle(new Style().setColor(TextFormatting.RED));
+		txt.appendSibling(txt2);
+		sender.sendMessage(txt);
+
+		txt = new TextComponentString("Voir : ");
+		txt.setStyle(new Style().setColor(TextFormatting.RED));
+
+		txt2 = new TextComponentString("/kit help");
+		txt2.setStyle(new Style().setBold(true).setColor(TextFormatting.DARK_BLUE).setClickEvent(new ClickEvent(Action.SUGGEST_COMMAND, "/kit help")));
+
+		txt.appendSibling(txt2);
+
+		sender.sendMessage(txt);
+
 	}
 
 	@Override
