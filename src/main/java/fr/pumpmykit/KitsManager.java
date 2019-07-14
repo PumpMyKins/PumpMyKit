d8ad9e6b7363bca1b0c36473287c0c134d43576a
Clem-Fern <clemfern456@gmail.com>
2019-07-14 03:21:00 +0200

    Update imports in KitsManager class

--- src/main/java/fr/pumpmykit/KitsManager.java
+++ src/main/java/fr/pumpmykit/KitsManager.java
@@ -20,7 +20,12 @@ import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.inventory.InventoryBasic;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.items.ItemHandlerHelper;
 
 public class KitsManager {
 
